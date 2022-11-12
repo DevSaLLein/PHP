d1e3cda1039681dfdb9d7339e522e31ad6994622
DevSaLLein <112987989+DevSaLLein@users.noreply.github.com>
2022-11-12 18:18:22 -0300

    Quarta Prova finalizada!

--- Escola/QuartoBim + LinkFour/Prova/home.php
+++ Escola/QuartoBim + LinkFour/Prova/home.php
@@ -4,18 +4,30 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Prova do Quarto Bimestre</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <?php
             echo "
-                <form method='POST'>
-                    <label for='cliente'>Nome do Cliente: </label>
-                    <input type='text' id='cliente' name='cliente'/> <br>
+                <div class='content'>
+                <fieldset>
+            ";
+        ?>
+        NumCliente:
+        <?php
+            $idCliente = rand(1, 100000);
+            echo $idCliente;
+            echo " 
+                <br>
+                    <legend>Comprar</legend>
+                    <form method='POST'>
+                        <label for='cliente'>Nome do Cliente: </label>
+                        <input type='text' id='cliente' name='cliente'/> <br>
 
-                    <label for='litros'>Qtd em Litros: </label>
-                    <input type='number' id='litros' name='litros'/> <br>
+                        <label for='litros'>Qtd em Litros: </label>
+                        <input type='number' id='litros' name='litros'/> <br>
 
-                    <label for='prod'>Combustível</label>
+                        <label for='prod'>Combustível</label>
             ";
 
             $arr = array(
@@ -33,43 +45,47 @@
             }
 
             echo "
-                    <input type='submit' value='Calcular'/>
-                </form>
+                        <input type='submit' value='Calcular'/>
+                    </form>
+                </fieldset>
             ";
 
         ?>
 
         <?php
-            // $arrValores = array();
-            // foreach($arr as $key => $values){
-            //     foreach($arrValores as $chave => $valores){
-            //         $arrValores[] = $values;
-            //     }
-            // }
 
-            // print_r($arrValores);
-            
+            $arrValores = array(
+                3.99,
+                4.99,
+                10
+            );
 
-            $idCliente = rand(1, 100000);
             $cliente = $_POST['cliente'] ? $_POST['cliente']: 'Desconhecido';
         
             $prod = $_POST['prod'];
 
-            // $prod = explode(',', $prod);
-            // $nomeProd;
-            // foreach($arr as $key => $value){
-            //     foreach($prod as $chave => $valor){
-            //         if($value == $valor){
-            //              $nomeProd = $key;
-            //         }
-            //     }
-            // }
+            $prod = explode(',', $prod);
+            $nomeProd;
+
+            foreach($arr as $key => $value){
+                foreach($prod as $chave => $valor){
+                    if($value == $valor){
+                         $nomeProd = $key;
+                    }
+                }
+            }
 
             $litros = $_POST['litros'];
             $total = 0;
             $pontuacao = 0;
             
-            $total = $prod * $litros;
+            foreach($prod as $key => $value){
+                foreach($arrValores as $chave => $valor){
+                    if($value == $valor){
+                        $total = $valor * $litros;
+                    }
+                }
+            }
             
 
             $pontuacao = round($total * 2);
@@ -78,12 +94,27 @@
 
             $fp = fopen($nome_arquivo, 'w+');
 
+                fwrite($fp, "Id: ".$idCliente. "\n");
                 fwrite($fp, "Nome: ".$cliente. "\n");
-                fwrite($fp, "Combustível: ". $prod. "\n");      
-                fwrite($fp, "Total à pagar: ".$total. "\n");
+                fwrite($fp, "Combustível: ". $nomeProd. "\n");      
+                fwrite($fp, "Total à pagar: R$".number_format($total, '2', '.', ','). "\n");
                 fwrite($fp, "Pontuação: ".$pontuacao. "\n");
 
             fclose($fp);
+
+            echo "
+                <fieldset>
+                <legend>Dados</legend>
+            ";
+            $arquivo = file($nome_arquivo);
+            foreach($arquivo as $key => $line){
+                echo $line."<br>";
+            }
+            
+            echo "
+                </fieldset>
+                </div>
+            ";
 
         ?>
     </body>
